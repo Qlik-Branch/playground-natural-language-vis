f167f8498033c00bc2a3f3a613501f23e50a2239
Nick Webster <nick@websy.io>
2018-10-23 09:24:30 +0100

    remove capability apis, switch to enigma

--- resources/script.js
+++ resources/script.js
@@ -1,38 +1,43 @@
-var config={};
+let config = {}
+Playground.notify({
+	title: "Please wait...",
+	message: "Connecting"
+})
+// Change the schema version below as necessary to match your environment
+getSchema("12.170.2").then(schema => {
+	config.schema = schema
+	return Playground.authenticate(config, "ticket")
+}).then(function(ticket){
+  config.url += `?qlikTicket=${ticket}`
+	const session = enigma.create(config)
+  session.open().then(global => {		
+		global.openDoc(config.appname).then(app => {
+			senseSearch.usePicasso = true
+      senseSearch.connectWithEnigma(app)
+      var inputOptions = {
+         includeMasterMeasures: true,
+				 placeholder: 'What would you like to know?'
+      }
+      senseSearch.inputs["searchInput"].attach(inputOptions)
+      // senseSearch.inputs["searchInput"].addFieldTag("month", "$time");
+      senseSearch.results["searchResults"].onUnsupportedVisualization.subscribe(obj => {
+        Playground.notification.deliver({
+					title: "Not supported",
+					message: "The specified visualization is not currently supported",
+					duration: 3000
+				})				
+      })
+		})    
+  })
+})
 
-function authenticate(){
-  Playground.authenticate(config);
-}
-
-
-function main(){
-	Playground.notification.deliver({
-		title: "Please wait...",
-		message: "Connecting"
-	});
-  require.config({
-    baseUrl: ( config.isSecure ? "https://" : "http://" ) + config.host + (config.port ? ":" + config.port: "") + config.prefix + "resources",
-  });
-
-  require(['js/qlik'], function(qlik) {
-		Playground.notification.deliver({
-			title: "Ready",
-			message: "Connecting",
-			duration: 200
-		});
-    qlik.setOnError( function ( error ) {
-      console.log(error);
-			Playground.notification.deliver({
-				title: "Error",
-				message: error
-			});
-    });
-    console.log(qlik);
-    var app = qlik.openApp(config.appname, config);
-    console.log(app);
-    app.model.waitForOpen.promise.then(function(){
-       senseSearch.connectWithCapabilityAPI(app);
-    });
-  });
-
+function getSchema(version) {
+	return new Promise((resolve, reject) => {
+	    const xhr = new XMLHttpRequest()
+	    xhr.open("GET", `/node_modules/enigma.js/schemas/${version}.json`)
+			xhr.setRequestHeader("Access-Control-Allow-Origin","http://localhost:8000");
+	    xhr.onload = () => resolve(JSON.parse(xhr.responseText))
+	    xhr.onerror = () => reject(xhr.statusText)
+	    xhr.send()
+	  })
 }
